5946f633c62d8f68bbd8487c4ee9660f6e17cfb1
SUMIT BHATI <sumitbhati1406@gmail.com>
2026-06-07 16:07:56 +0530

    Update Service.js

--- backend/src/models/Service.js
+++ backend/src/models/Service.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const serviceSchema = new mongoose.Schema({
   name: {
@@ -39,4 +39,4 @@ const serviceSchema = new mongoose.Schema({
   order: { type: Number, default: 0 }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Service', serviceSchema);
+export default mongoose.model('Service', serviceSchema);
